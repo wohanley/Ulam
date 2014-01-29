ef5adc3b821b28bb175594f465fe58b743ae8cb7
William O'Hanley <willy.ohanley@gmail.com>
2014-01-28 19:51:32 -0500

    add trace marker to demo

--- js/start.js
+++ js/start.js
@@ -9,7 +9,7 @@ $(function () {
 	var plot = new ulam.draw.TwoDPlot(canvas, { stepLength: 10, symbolSize: 10 });
 	var primeMarker = new ulam.draw.markers.TwoPartMarker(ulam.math.isPrime, ulam.draw.symbols.Rectangle);
 	var pathMarker = new ulam.draw.markers.WalkTraceMarker();
-	var drawer = new ulam.draw.SquareSpiralWalker(plot, sequence, { markers: [ primeMarker, pathMarker ] });
+	var drawer = new ulam.draw.SquareSpiralWalker(plot, sequence, { markers: [ pathMarker, primeMarker ] });
 	
 	drawer.walk();
 });
