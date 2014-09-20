17a43241e793f7d7f4e60ae1f739f31b2339b0ee
William O'Hanley <liam@wohanley.com>
2014-09-20 13:32:42 +1200

    undo weird squarey thing

--- js/draw/symbols/Rectangle.js
+++ js/draw/symbols/Rectangle.js
@@ -1,18 +1,14 @@
-ulam.draw.symbols.Rectangle = function () {};
-ulam.draw.symbols.Rectangle.prototype.draw = function (drawingContext, point, dimensions) {
-	drawingContext.fillRect(point.x - dimensions.width / 2,
-		point.y - dimensions.height / 2,
-		dimensions.width,
-		dimensions.height);
-};
+ulam.draw.symbols.Rectangle = (function () {
 
-
-ulam.draw.symbols.SizeRectangle = function (n) {
-	this.draw = function (drawingContext, point, dimensions) {
-		var size = 10 * n;
-		drawingContext.strokeRect(point.x - size / 2,
-				point.y - size / 2,
-				size,
-				size);
+	var Rectangle = function () {};
+	
+	Rectangle.prototype.draw = function (drawingContext, point, dimensions) {
+		drawingContext.fillRect(point.x - dimensions.width / 2,
+			point.y - dimensions.height / 2,
+			dimensions.width,
+			dimensions.height);
 	};
-};
+	
+	return Rectangle;
+	
+})();
--- js/start.js
+++ js/start.js
@@ -1,6 +1,6 @@
 $(function () {
 	
-	var canvasSize = 500;
+	var canvasSize = 300;
 	
 	canvas = $('<canvas/>').appendTo('body');
 	canvas = canvas.get(0);
@@ -9,17 +9,13 @@ $(function () {
 	
 	var context = canvas.getContext("2d");
 	context.save();
-	context.fillStyle = "#FFF";
+	context.fillStyle = "#001";
 	context.fillRect(0, 0, canvasSize, canvasSize);
-	context.fillStyle = "#000";
 	
-	var sequence = new ulam.sequence.ArithmeticSequence({ end: 100 });
-	var plot = new ulam.draw.TwoDPlot(canvas, { stepLength: 10, symbolSize: 8 });
-	var primeMarker = new ulam.draw.markers.TwoPartMarker(ulam.math.isPrime, ulam.draw.symbols.Rectangle);
-	//var pathMarker = new ulam.draw.markers.WalkTraceMarker();
-	var allMarker = new ulam.draw.markers.TwoPartMarker(function () { return true; }, ulam.draw.symbols.SizeRectangle);
-	var drawer = new ulam.draw.SquareSpiralWalker(plot, sequence, { markers: [ allMarker ]});
-	//var drawer = new ulam.draw.HexagonalSpiralWalker(plot, sequence, { markers: [ primeMarker ] });
+	var sequence = new ulam.sequence.ArithmeticSequence({ end: 29 * 29 });
+	var plot = new ulam.draw.TwoDPlot(canvas, { stepLength: 10, symbolSize: 6 });
+	var primeMarker = new ulam.draw.markers.TwoPartMarker(ulam.math.isPrime, ulam.draw.symbols.getTwinklingStarConstructor(5, "#eee", "#000"));
+	var drawer = new ulam.draw.HexagonalSpiralWalker(plot, sequence, { markers: [ primeMarker ] });
 	
 	drawer.walk();
 });
