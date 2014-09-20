8bf0dca92b662a4a0a1e2598f9e1f72b4359c464
William O'Hanley <liam@wohanley.com>
2014-09-20 13:28:29 +1200

    this is kind of cool

--- js/draw/symbols/Rectangle.js
+++ js/draw/symbols/Rectangle.js
@@ -1,14 +1,18 @@
-ulam.draw.symbols.Rectangle = (function () {
+ulam.draw.symbols.Rectangle = function () {};
+ulam.draw.symbols.Rectangle.prototype.draw = function (drawingContext, point, dimensions) {
+	drawingContext.fillRect(point.x - dimensions.width / 2,
+		point.y - dimensions.height / 2,
+		dimensions.width,
+		dimensions.height);
+};
 
-	var Rectangle = function () {};
-	
-	Rectangle.prototype.draw = function (drawingContext, point, dimensions) {
-		drawingContext.fillRect(point.x - dimensions.width / 2,
-			point.y - dimensions.height / 2,
-			dimensions.width,
-			dimensions.height);
+
+ulam.draw.symbols.SizeRectangle = function (n) {
+	this.draw = function (drawingContext, point, dimensions) {
+		var size = 10 * n;
+		drawingContext.strokeRect(point.x - size / 2,
+				point.y - size / 2,
+				size,
+				size);
 	};
-	
-	return Rectangle;
-	
-})();
+};
